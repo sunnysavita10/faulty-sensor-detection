9b090c1bf07fd430a0c117d8957d2b580dba14b0
sunnysavita10 <engsunny3365@gmail.com>
2022-11-03 18:15:14 +0530

    update requirement and setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+name="sensorfalut",
+version="0.0.1", 
+author="ineuron.ai", 
+author_email="sunny.savita@ineuron.ai",
+package=find_packages(),
+install_requires=[]
+)
