7a47959398ff8607e54f0951c841cb96b898d408
sunnysavita10 <engsunny3365@gmail.com>
2022-11-03 23:30:04 +0530

    notebook updated

---
+++ sensor/logging.py
@@ -0,0 +1,4 @@
+import logging
+import datetime
+import os
+
--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 from setuptools import setup, find_packages
 
+REQUIREMENT_FILE_NAME = "requirements.txt"
+REMOVE_PACKAGE = "-e ."
+
+
+def get_requirement_list(requirement_file_name=REQUIREMENT_FILE_NAME) -> list:
+    try:
+        requirement_list = None
+        with open(requirement_file_name) as requirement_file:
+            requirement_list = [requirement.replace("\n", "") for requirement in requirement_file]
+            requirement_list.remove(REMOVE_PACKAGE)
+        return requirement_list
+    except Exception as e:
+        raise e
+
+
 setup(
-name="sensorfalut",
-version="0.0.1", 
-author="ineuron.ai", 
-author_email="sunny.savita@ineuron.ai",
-package=find_packages(),
-install_requires=[]
+    name="sensor fault detection",
+    license="MIT",
+    version="0.0.0",
+    description="Project has been completed.",
+    author="sunny savita",
+    packages=find_packages(),
+    install_requires=get_requirement_list()
 )
