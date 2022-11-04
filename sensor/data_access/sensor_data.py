5417868f5cdac8ae312f4de84bc233b25f16c638
sunnysavita10 <engsunny3365@gmail.com>
2022-11-04 16:34:48 +0530

    data access file updated

---
+++ sensor/data_access/sensor_data.py
@@ -0,0 +1,23 @@
+import sys
+from typing import Optional
+import numpy as np
+import pandas as pd
+
+
+from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.constant.database import DATABASE_NAME
+from sensor.exception import SensorException
+
+class SensorData:
+    def __init__(self):
+        try:
+            pass
+            
+        except:
+            pass
+    def export_collection_as_dataframe() :
+        try:
+            pass
+        
+        except:
+            pass
